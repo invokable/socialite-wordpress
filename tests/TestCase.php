42582170056eadfcae136f42f1a0061cd49aee74
kawax <kawaxbiz@gmail.com>
2026-04-14 20:21:31 +0900

    wip

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -40,14 +40,6 @@ class TestCase extends \Orchestra\Testbench\TestCase
                 'redirect' => 'http://localhost',
             ],
         );
-
-        // Setup default database to use sqlite :memory:
-        $app['config']->set('database.default', 'testbench');
-        $app['config']->set('database.connections.testbench', [
-            'driver' => 'sqlite',
-            'database' => ':memory:',
-            'prefix' => '',
-        ]);
     }
 
     protected function defineWebRoutes($router): void
